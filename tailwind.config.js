73a88bb60b0992cf38093693fe24b84683d4e14d
DomN <127251733+DominikN97@users.noreply.github.com>
2024-04-23 08:36:48 +0000

    new

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'green-dark': '#88AB8E',
+      'green-light': '#AFC8AD',
+      'grey': '#F2F1EB',
+      'beige-light': '#EEE7DA'
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
